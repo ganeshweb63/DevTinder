614f39b6c82f76c1031ae939d2fbaac52480356d
Ganesh Asimshetti <ganeshweb63@gmail.com>
2025-02-22 00:24:03 +0530

    handle request params.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use("/test", (req, res) => {
-  res.send("This is testing");
+app.get("/hello", (req, res) => {
+  const name = req.query.name;
+  const response = `This is hello from ${name}`;
+  console.log(response);
+  res.send(response);
 });
-
+app.get("/age/:age", (req, res) => {
+  const age = req.params.age;
+  console.log(req.params);
+  const response = `This is my age  ${age}`;
+  console.log(response);
+  res.send(response);
+});
 /// Last Route
 app.use("/", (req, res) => {
   res.send("Welcome to Dev Tinder.");
 });
 
 app.listen(port, () => {
-  console.log(`Server started succesfully on ${port}`);
+  console.log(`Server started successfully on ${port}`);
 });
